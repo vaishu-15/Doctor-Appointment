62013fea2c6a5bce74047133ff38a6356d1e866b
vaishu-15 <vaishali15yadav@gmail.com>
2024-04-01 12:40:25 +0530

    files created

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StatusBar, View} from 'react-native';
+import Routes from './src/navigation/Routes';
+
+const App = props => {
+  return (
+    <View style={{flex: 1}}>
+      <StatusBar backgroundColor={'black'} translucent />
+      <Routes/>
+    </View>
+  );
+};
+
+export default App;
---
+++ src/screen/Schedule/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Schedule=() =>{
+return (
+<Text>
+    Chat
+</Text>
+);
+}
+
+export default Schedule;
